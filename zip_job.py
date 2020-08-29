9b9215bed5ca5155eddb70b2614ca304b4786a34
ronlevy1211 <ronlevy1997@gmail.com>
2020-08-29 11:53:11 +0300

    added logs to the script

--- zip_job.py
+++ zip_job.py
@@ -2,18 +2,15 @@ import sys
 import zipfile
 import os
 
-print("*******************")
-print("**START*ZIP*BUILD**")
-print("*******************")
 
 list = ['a','b','c','d']
 for name in list:
     try:
-        f = open('./{}.txt'.format(name),'w')
+        f = open('/tmp/zip_builds/{}.txt'.format(name),'w')
     except:
         sys.exit("Filad to create {}.txt".format(name))
     f.close()
     try:
-        zipfile.ZipFile('{}_{}.zip'.format(name, os.environ['VERSION']), mode='w').write("{}.txt".format(name))
+        zipfile.ZipFile('/tmp/zip_builds/{}_{}.zip'.format(name, os.environ['VERSION']), mode='w').write("/tmp/zip_builds/{}.txt".format(name))
     except:
         sys.exit("Filad to create {}_{}.zip".format(name, os.environ['VERSION']))
